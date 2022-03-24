a67e02af23c7b8fe0263ca5c66299acbae16d511
Arturo Cervantes <jesus.cervantes207003@potros.itson.edu.mx>
2022-03-23 21:44:57 -0700

    Cambio a ejs para implementacion de codigo JS

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ const app = express();
 const path = require('path');
 
 app.set('port', 3001);
+app.engine('view engine', 'ejs');
 
 app.listen(app.get('port'), () =>{
     console.log('Server on port', app.get('port'));
 })
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, './views/index.html'))
+    res.render('index.ejs')
 })
