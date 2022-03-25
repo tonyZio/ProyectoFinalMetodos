189c88b427e970c42de0e98e3ffc58c1f96fa24d
Arturo Cervantes <jesus.cervantes207003@potros.itson.edu.mx>
2022-03-24 19:01:55 -0700

    Conexion entre index y node

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const app = express();
 const path = require('path');
 
 app.set('port', 3001);
-app.engine('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
 
 app.listen(app.get('port'), () =>{
     console.log('Server on port', app.get('port'));
 })
 
 app.get('/', (req, res) =>{
-    res.render('index.ejs')
+    res.render('index')
 })
